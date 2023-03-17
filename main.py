cb24e8236ec6e0bf5d66cda74abfc7dcdb1c3a29
linqing <2458029060@qq.com>
2023-03-17 22:55:46 +0800

    update

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ background_size = width,height = 480,700
 screen = pygame.display.set_mode(background_size)
 pygame.display.set_caption('起飞')
 
-background_image = pygame.image.load('')
+background_image = pygame.image.load(r'D:\python\x\plane-war\image\sky.jpeg').convert()
 
+background_image.blit()
