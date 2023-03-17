8cd884073d9005d32d8cc87c41390a4f3006d610
linqing <2458029060@qq.com>
2023-03-17 22:42:13 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 import sys
+import traceback
 
+#####初始化#####
 pygame.init()
-pygame.display.set_mode(480,700)
+pygame.mixer.init()
+
+background_size = width,height = 480,700
+screen = pygame.display.set_mode(background_size)
+pygame.display.set_caption('起飞')
+
+background_image = pygame.image.load('')
 
