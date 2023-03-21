0d288937cf6c643cf69d6aaa11d58e7adf8f3e69
linqing0 <2458029060@qq.com>
2023-03-21 15:05:46 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import pygame
 import sys
 import traceback
-
+os.chdir(r'C:\app\python\X\plane-war')
 #####初始化#####
 pygame.init()
 pygame.mixer.init()
@@ -10,5 +11,4 @@ background_size = width,height = 480,700
 screen = pygame.display.set_mode(background_size)
 pygame.display.set_caption('plane war')
 
-background_image = pygame.image.load('')   #convert 变换background透明度
-
+# background_image = pygame.image.load(r'C:\app\python\X\plane-war\image\sky.jpeg').convert()  #convert 变换background透明度
