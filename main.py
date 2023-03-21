1125e1e33993dfc78ac3e316e22c7b0f6aa5e35f
linqing0 <2458029060@qq.com>
2023-03-21 10:54:43 +0800

    update

--- main.py
+++ main.py
@@ -10,6 +10,5 @@ background_size = width,height = 480,700
 screen = pygame.display.set_mode(background_size)
 pygame.display.set_caption('plane war')
 
-background_image = pygame.image.load(r'D:\python\x\plane-war\image\sky.jpeg').convert()    #convert 变换background透明度
+background_image = pygame.image.load('')   #convert 变换background透明度
 
-background_image.blit()
