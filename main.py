430274152287563c2f2ff4f530663d6cbbf80280
linqing <2458029060@qq.com>
2023-03-22 22:58:37 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import pygame
 import sys
 import traceback
@@ -12,7 +11,34 @@ screen = pygame.display.set_mode(background_size)
 pygame.display.set_caption('plane war')
 
 '''----- image -----'''
-background_image = pygame.image.load(r'./image/sky.jpeg').convert()    #convert转换image的透明度
+background_image = pygame.image.load(r'./image/sky.jpeg')    #convert转换image的透明度
 
 '''----- sound -----'''
-background_music = pygame.mixer
+pygame.mixer.music.load(r'./sound/春花.mp3')
+
+
+
+#####主程序#####
+def main():
+    pygame.mixer.music.play(-1)
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.quit():
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(background_image,(0,0))
+        pygame.display.flip()
+        clock.tick(60)    #设置成60帧
+
+if __name__ == '__main__':
+    try:
+        main()
+    except SystemExit:
+        pass
+    except:
+        traceback.print_exc()
+        pygame.quit()
+        input()
