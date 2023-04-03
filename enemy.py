68e0c1302073f1243813ac3b58a81e8a853be689
linqing <2458029060@qq.com>
2023-04-03 11:30:29 +0800

    update

--- enemy.py
+++ enemy.py
@@ -1,12 +1,15 @@
 import pygame
+from random import *
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,backfground_size):
         pygame.sprite.Sprite.__init__()
         self.enemy1 = pygame.image.load(r'./image/enemy_lv1.png')
         self.rect1 = self.enemy1.get_rect()
-
-
+        self.width ,self.height = backfground_size[0],backfground_size[1]
+        self.speed = 2
+        self.rect1.left , self.rect1.top =randint(self.width - self.width)
 
         self.enemy2 = pygame.image.load(r'./image/enemy_lv2.png')
         self.enemy3 = pygame.image.load(r'./image/enemy_lv3.png')
