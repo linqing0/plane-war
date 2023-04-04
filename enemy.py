b05e4e2356f44f7308a260cc9bd24db7633d2790
linqing0 <2458029060@qq.com>
2023-04-04 11:49:01 +0800

    update

--- enemy.py
+++ enemy.py
@@ -5,8 +5,8 @@ from random import *
 class Enemy_Lv1(pygame.sprite.Sprite):
     def __init__(self,backfground_size):
         pygame.sprite.Sprite.__init__(self)
-        self.enemy = pygame.image.load(r'./image/enemy_lv1.png')
-        self.rect = self.enemy.get_rect()
+        self.image = pygame.image.load(r'./image/enemy_lv1.png')
+        self.rect = self.image.get_rect()
         self.width ,self.height = backfground_size[0],backfground_size[1]
         self.speed = 3
         self.rect.left , self.rect.top = randint(0,self.width - self.width) , randint(-3 * self.height, 0)    #进场时机
@@ -21,8 +21,8 @@ class Enemy_Lv1(pygame.sprite.Sprite):
 class Enemy_Lv2(pygame.sprite.Sprite):
     def __init__(self,backfground_size):
         pygame.sprite.Sprite.__init__(self)
-        self.enemy = pygame.image.load(r'./image/enemy_lv2.png')
-        self.rect = self.enemy.get_rect()
+        self.image = pygame.image.load(r'./image/enemy_lv2.png')
+        self.rect = self.image.get_rect()
         self.width ,self.height = backfground_size[0],backfground_size[1]
         self.speed = 2
         self.rect.left , self.rect.top = randint(0,self.width - self.width) , randint(-8 * self.height, -self.height)
@@ -37,8 +37,8 @@ class Enemy_Lv2(pygame.sprite.Sprite):
 class Enemy_Lv3(pygame.sprite.Sprite):
     def __init__(self,backfground_size):
         pygame.sprite.Sprite.__init__(self)
-        self.enemy = pygame.image.load(r'./image/enemy_lv3.png')
-        self.rect = self.enemy.get_rect()
+        self.image = pygame.image.load(r'./image/enemy_lv3.png')
+        self.rect = self.image.get_rect()
         self.width ,self.height = backfground_size[0],backfground_size[1]
         self.speed = 1
         self.rect.left , self.rect.top = randint(0,self.width - self.width) , randint(-13 * self.height, -5 * self.height)
